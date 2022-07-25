0c8591571b7274ad5b03d5926778fdde4ae3d7f8
Nguyen Minh Tuan <tuannm88.hni@gmail.com>
2022-07-25 20:47:24 +0700

    Create array-constructor.js

---
+++ javascript-25-07-2/c02/js/array-constructor.js
@@ -0,0 +1,7 @@
+//Create and name the variable.
+//Tell the interpreter it is an array.
+//Assign values inside the parentheses.
+var color = new Array ('white','black', 'custom');
+//Show the first item from the array.
+var el=document.getElementById('colors');
+el.textContent=color[0];
