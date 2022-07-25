f8c92253ad940f74e74a487bf8ba32054465ef43
Nguyen Minh Tuan <tuannm88.hni@gmail.com>
2022-07-25 20:47:35 +0700

    Create example.js

---
+++ javascript-25-07-2/c02/js/example.js
@@ -0,0 +1,15 @@
+el.textContent = welcome;
+var elSign=document.getElementById('userSign');
+elSign.textContent=sign;
+
+var elTiles=document.getElementById('tiles');
+elTiles.textContent=tiles;
+
+var elSubTotal=document.getElementById('subtotal');
+elSubTotal.textContent='$'+ subTotal;
+
+var elShipping=document.getElementById('shipping');
+elShipping.textContent='$'+shipping;
+
+var elGrandTotal=document.getElementById('grandTotal');
+elGrandTotal.textContent="$" + elGrandTotal;
